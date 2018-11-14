79b99bda0f052338a3be4b650ddd8b56428a40d6
rpilleux <44799338+rpilleux@users.noreply.github.com>
2018-11-14 23:57:28 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,32 +1,41 @@
-#include "TP_lib.h"
-#include "TP_lib.c" 
+#include "stdlib.h"
+#include <math.h>
+#define RAND_MAX 32767
 
-	short resultat; 
-	unsigned short points=0;
+int n=1;
+long long S1=0;
+int x;
+int a=0;
+int val=0;
+int vol=0;
+int y=1;
+int z=0;
 
+int main(void)
 
-void main(void)
+{  
 
-{
-	
-	while(resultat!=1)
-		
-	{
-		 resultat=
-		LancerDe();
-		 
-		if(resultat==2 ||    resultat==4 || resultat==6)
-			
-			points=resultat;
-		
-		else if(resultat==3)
-			
-			points=points*2;
-		
-		else if(resultat==5)
-		
-			points=points-2;
-			
-	}
-	
+   while (S1<9876)
+   {
+	   S1=S1+3*n;
+		  n=n+1;
+   }
+   
+   while (a<12)
+   { val=val+a;
+	   a=a+1;
+		 x=((rand())%10)+1;
+		   
+   }
+		   
+		   while(z<20)
+   {
+	   if (y%3==0  ||  y%7==0)
+	   {
+		 vol=vol+y;
+		 y=y+1;
+		 z=z+1;
+	   }
+	   else y=y+1;
+	   }
 }
