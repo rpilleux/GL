f2c69096b45a0b454b0a942268384f8bbfb76566
rpilleux <44799338+rpilleux@users.noreply.github.com>
2018-12-05 22:45:33 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <userint.h>
+#include <ansi_c.h>
+#include <stdlib.h>
 
-#include "my_header.h"
+
+
 
 int main(void)
-{
+{   char phrase[100];
+    char prenom[100];
+	char nom[100];
+	unsigned char age=0;
+	
+	printf("Entrer votre prenom svp:");
+	scanf("%s", prenom);
+	printf("Entrer votre nom svp:");
+	scanf("%s", nom);
+    printf("Entrer votre age svp:");
+	scanf("%u", &age);
+	sprintf(phrase,"je m'appelle %s %s et j'ai %u ans", prenom, nom, &age);
+	MessagePopup("resultat", phrase);
+
     return 0;
 }
